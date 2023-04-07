d5dfb0601229339d09b7f419452fab579c121ed9
Maegan Phan <maegyoung@gmail.com>
2023-04-07 12:13:18 -0500

    deleted original navbar components because nav2 is being used, deleted tripedit and triplist because combined into dynamic edit form and list.

--- src/components/howToPlay/howToPlay.js
+++ src/components/howToPlay/howToPlay.js
@@ -18,7 +18,7 @@ export const HowToPlay = () => {
 					<div className="howToPlay">
 					<h2 className="playHeader">How To Play the Maryland Bay Game:</h2>
                     <h4 className="playNote">Note: The game is designed to be played in the car as you ride to Ocean City via Rt. 50</h4>
-					<p className="playRules">1. Start looking <b>NOW</b> for rivers and streams that you cross on the way to Ocean City. All have been marked with a sign. One activity challenges you to find all these rivers and streams.</p>
+					<p className="playRules">1. Start looking <b>NOW</b> for <Link className="link" to="/riversandstreams">rivers and streams</Link> that you cross on the way to Ocean City. All have been marked with a sign. One activity challenges you to find all these rivers and streams.</p>
                     <p className="playRules">2. On the <Link className="link" to="/bayitems">Scavenger Hunt</Link> page, there are 24 Bay related things that you are challenged to find on your trip. When you spot an object, click the "Found it!" button. Try to find all of the Bay related items.</p>
                     <p className="playRules">3. When the trip is over, you can click the plus sign on the Scavenger Hunt Page to <Link className="link" to="/recordoftrips/new">Add a New Trip</Link>. You can record the date, name the trip, occasion, and how many bay related items you found. When a new trip is added, it will appear under your Record of Trips on the Scavenger Hunt Page!</p>
                     <p className="playRules">4. You can reset all Scavenger Hunt items by clicking the "reset" button so you can play again on your next road trip.</p>
